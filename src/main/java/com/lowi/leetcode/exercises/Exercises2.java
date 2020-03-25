8147449ef9110ba56d2e215bc5d9f4a7ce0f7826
gengyy <gengyy@51lick.cn>
2020-03-25 11:05:24 +0800

    首次提交

---
+++ src/main/java/com/lowi/leetcode/exercises/Exercises2.java
@@ -0,0 +1,17 @@
+package com.lowi.leetcode.exercises;
+
+/**
+ * Exercises2.java
+ * ==============================================
+ * Copy right 2015-2017 by http://www.51lick.com
+ * ----------------------------------------------
+ * This is not a free software, without any authorization is not allowed to use and spread.
+ * ==============================================
+ *
+ * @author : gengyy
+ * @version : v2.0
+ * @desc :
+ * @since : 2020/3/25 11:05
+ */
+public class Exercises2 {
+}
